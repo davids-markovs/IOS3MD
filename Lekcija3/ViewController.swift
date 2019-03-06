9df0a2c387be35a191676b056dea599a3c0f8b43
students <students@studentss-imac.local>
2019-03-06 16:41:20 +0200

    Initial Commit

---
+++ Lekcija3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lekcija3
+//
+//  Created by Students on 06/03/2019.
+//  Copyright © 2019 students. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
